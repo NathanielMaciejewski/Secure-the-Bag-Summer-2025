014ab4b0f620a6d28ddc80063d39043b6aaaf791
BB-Driver00 <168240866+BB-Driver00@users.noreply.github.com>
2025-07-22 00:36:08 -0700

    Update VOTrigger.cs

--- Assets/Scripts/Audio/VOTrigger.cs
+++ Assets/Scripts/Audio/VOTrigger.cs
@@ -7,17 +7,25 @@ public class VOTrigger : MonoBehaviour
     [field: Header("Parameter Change")]
     [field: SerializeField] public EventReference voicOverLine { get; private set; }
 
+    private bool hasPlayed;
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        hasPlayed = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.tag.Equals("Player"))
+        if (hasPlayed == false)
         {
-            RuntimeManager.PlayOneShot(voicOverLine);
-            Destroy(gameObject);
+            if (collider.tag.Equals("Player"))
+            {
+                RuntimeManager.PlayOneShot(voicOverLine);
+            }
         }
+        hasPlayed = true;
+        return;
+
     }
 }
